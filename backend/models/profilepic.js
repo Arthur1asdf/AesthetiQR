2e26bef3366fdee7a6f43b4d6f29dcf55a9e7850
Anna Zheng <anna.zheng@ucf.edu>
2025-03-27 18:16:48 -0400

    actually pushing port change to 3000 for backend

--- backend/models/profilepic.js
+++ backend/models/profilepic.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
 
 const ProfilePicSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  imageUrl: { type: String, required: true },
-  uploadedAt: { type: Date, default: Date.now },
+  user: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User", required: true 
+  },
+  imageUrl: { 
+    type: String, required: true 
+  },
+  uploadedAt: { 
+    type: Date, 
+    default: Date.now 
+  },
 });
 
 export default mongoose.model("ProfilePic", ProfilePicSchema);
